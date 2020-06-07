bfdc8c1707184456057c889f5383eddfad567f8d
Chalanika <chalanikasewwandi@gmail.com>
2020-06-07 09:43:11 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={2}>
+        hello world hhhhhhhhhhhhhhhhhhhhhhh bjhkjji njchhxij skjiosa mxjxhisu sjkckhcidj cjhdjciii hello world hhhhhhhhhhhhhhhhhhhhhhh bjhkjji njchhxij skjiosa mxjxhisu sjkckhcidj cjhdjciii
+      </Text>
+      <Image source={{
+        width:200,
+        height:300, 
+        uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGmPW_Scx2vfFBt4MWqD3bd7zck4l00fMoH7ynwNoLHnlcxWjC&usqp=CAU"
+        }}
+      />
+           
     </View>
   );
 }
@@ -12,8 +21,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#A6A9BC',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+    justifyContent:"center",
+    alignItems:"center",
+    
+},
 });
