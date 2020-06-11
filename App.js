504e4bfc678ff7230d7875d516c469e165656407
Chalanika <chalanikasewwandi@gmail.com>
2020-06-11 21:25:57 +0530

    added text input

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [value, onChangeText] = React.useState('E.g: Hasika Dilshani');
   return (
     <View style={styles.container}>
-      <Text numberOfLines={2}>
-        hello world hhhhhhhhhhhhhhhhhhhhhhh bjhkjji njchhxij skjiosa mxjxhisu sjkckhcidj cjhdjciii hello world hhhhhhhhhhhhhhhhhhhhhhh bjhkjji njchhxij skjiosa mxjxhisu sjkckhcidj cjhdjciii
+      <Text style={styles.title}>
+        Enter Your Name :
       </Text>
-      <Image source={{
-        width:200,
-        height:300, 
-        uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGmPW_Scx2vfFBt4MWqD3bd7zck4l00fMoH7ynwNoLHnlcxWjC&usqp=CAU"
-        }}
+      <TextInput
+        style={ styles.userInput }
+        onChangeText={text => onChangeText(text)}
+        value={value}
       />
-           
+      <Button style={{ height: 30}}
+        title="Submit"
+      />
     </View>
   );
 }
@@ -21,10 +23,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#A6A9BC',
-    paddingTop: Platform.OS === 'android' ? 25 : 0,
-    justifyContent:"center",
-    alignItems:"center",
-    
-},
+    backgroundColor: '#8EB6F4',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title:{
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+  userInput:{
+    borderColor: 'black', 
+    borderWidth: 2, 
+    padding: 5, 
+    margin: 20, 
+    marginBottom: 70, 
+    backgroundColor: '#CFD6E1'
+  }
+
 });
