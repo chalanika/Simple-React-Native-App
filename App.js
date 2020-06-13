fef2807d203ff1470acdbe339fdbe9b88e53fe05
Chalanika <chalanikasewwandi@gmail.com>
2020-06-13 14:59:00 +0530

    remove button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React ,{useState} from 'react';
-import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('');
